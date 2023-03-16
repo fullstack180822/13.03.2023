a3e078e3632a944c3f82824ff0f6e264e39e8598
micserv010821 <dotnet290320@gmail.com>
2023-03-16 16:37:13 +0200

    Update MyArrayList.java

--- coll2/src/main/java/collections2/example/MyArrayList.java
+++ coll2/src/main/java/collections2/example/MyArrayList.java
@@ -1,11 +1,17 @@
 package collections2.example;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MyArrayList <E> extends MyAbstractList<E>
         implements MyList<E>, GetNumber<Integer> {
 
     ArrayList<E> items;
+
+    void compare(E e) {
+        items.contains(e);
+
+    }
 
     @Override
     public E getItem(int index) {
